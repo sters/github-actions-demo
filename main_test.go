8324d76ec8200dfdcbc55710480f83f129093294
sters <sters@users.noreply.github.com>
2021-02-08 20:48:04 +0900

    improve say func

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func Test_say(t *testing.T) {
 	want := "I'm bot, hello!"
-	got := say("hello!")
+	got := say("bot", "hello!")
 
 	if want != got {
 		t.Errorf("want = %s, got = %s", want, got)
